0869f0e59676e955b1dc0851c9859e25256ac3a7
mihoko-churei <67135219+mihoko-churei@users.noreply.github.com>
2020-08-16 22:47:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // 「Hello World」とコンソールに出力してください
 console.log("Hello World");
+
+// 5と3を足した値を出力してください
+console.log(5 + 3);
+
+// 20から8を引いた値を出力してください
+console.log(20 - 8);
+
+// "4 + 5" を文字列として出力してください
+console.log("4 + 5");
