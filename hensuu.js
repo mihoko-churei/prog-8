6de818066f30eca7808c817ce961d683ee940589
mihoko-churei <67135219+mihoko-churei@users.noreply.github.com>
2020-08-17 23:16:02 +0900

    Update hensuu.js

--- hensuu.js
+++ hensuu.js
@@ -12,3 +12,34 @@ console.log(length);
 
 // 変数lengthを用いて、円の面積を出力してください
 console.log(length * length * 3);
+
+let name = "にんじゃわんこ";
+console.log(name);
+
+// 変数nameの値を"とりずきん"に更新してください
+name = "とりずきん";
+
+// 変数nameの値を出力してください
+console.log(name);
+
+let number = 7;
+console.log(number);
+
+// 変数numberの値に3を加えてください
+number = number + 3;
+
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number /= 2;
+
+console.log(number);
+
+// 定数languageを定義してください
+const language = "フランス語";
+
+// 定数languageの値を出力してください
+console.log(langage);
+
+// 定数languageを用いて、「〇〇を話せます」と出力してください
+console.log(langage + "を話せます");
