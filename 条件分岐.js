c80a866051286fa9a7396bea357a39706dc0bfa8
mihoko-churei <67135219+mihoko-churei@users.noreply.github.com>
2020-08-18 23:42:37 +0900

    Update 条件分岐.js

--- 条件分岐.js
+++ 条件分岐.js
@@ -5,6 +5,7 @@ if(level > 10){
   console.log("レベルが10より大きいです");
 }
 
+
 const age = 24;
 
 // 「age >= 20」を出力してください
@@ -16,4 +17,39 @@ console.log(age < 20);
 // ageの値が20以上の場合に、「私は20歳以上です」と出力してください
 if(age >= 20){
   console.log("私は20歳以上です");
+}
+
+
+const password = "ninjawanko";
+
+// passwordの値が"ninjawanko"の場合、「ログインに成功しました」と出力してください
+if(password === "ninjawanko"){
+  console.log("ログインに成功しました");
+}
+
+// passwordの値が"ninjawanko"でない場合、「パスワードが間違っています」と出力してください
+if(password !== "ninjawanko"){
+  console.log("パスワードが間違っています");
+}
+
+
+const age = 17;
+
+// 条件式が成り立たない場合に「私は20歳未満です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else{
+  console.log("私は20歳未満です");
+}
+
+
+const age = 17;
+
+// ageの値が10以上20未満のとき、「私は20歳未満ですが、10歳以上です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else if (age >= 10){
+  console.log("私は20歳未満ですが、10歳以上です"); 
+}else {
+  console.log("私は10歳未満です");
 }
