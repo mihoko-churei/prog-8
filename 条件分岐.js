f1493977b6f1354d7a31f7e7eabe7d1158b88b67
mihoko-churei <67135219+mihoko-churei@users.noreply.github.com>
2020-08-18 22:48:01 +0900

    Create 条件分岐.js

---
+++ 条件分岐.js
@@ -0,0 +1,19 @@
+const level = 12;
+
+// 条件式を「level > 10」とするif文を作ってください
+if(level > 10){
+  console.log("レベルが10より大きいです");
+}
+
+const age = 24;
+
+// 「age >= 20」を出力してください
+console.log(age >= 20);
+
+// 「age < 20」を出力してください
+console.log(age < 20);
+
+// ageの値が20以上の場合に、「私は20歳以上です」と出力してください
+if(age >= 20){
+  console.log("私は20歳以上です");
+}
